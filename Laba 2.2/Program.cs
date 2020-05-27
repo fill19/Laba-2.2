d70c1c4abb42e8c1abbcf251c2ebd11285a66276
Роман Данилевич <dwordchanel@gmail.com>
2020-05-27 16:58:04 +0300

    Add project files.

---
+++ Laba 2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Laba_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double x = -1 * Math.PI; x < Math.PI; x += Math.PI / 10)
+            {
+                double y = 5 * x * Math.Sin(x);
+                Console.WriteLine("y = {1:f4} x = {0:f4}", x, y);
+            }
+        }
+    }
+}
+
